2f6f89db76aa04110fd19266f0a8b753544af82f
Wollace Cavalcante Dantas <wollacecavalcante@outlook.com.br>
2023-07-12 23:05:58 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,8 +1,11 @@
 #calculadora em python
 while True:
-    x= int(input('Digite o primeiro número:'))
-    y= int(input('Digite o segundo número:'))
+    x= input('Digite o primeiro número:')
+    y= input('Digite o segundo número:')
     z= input('Digite a operação:')
+
+    x=int(x)
+    y=int(y)
 
     if z=='+':
         print('O resultado de x+y é igual a', x+y,'.')
